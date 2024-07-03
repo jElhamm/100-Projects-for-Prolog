ac55748ab3a06ee2c9060830275c9dca4c5ccf99
jelhamm <elhamjafari200312@gmail.com>
2024-07-04 00:45:33 +0430

    Add initial implementation of Graph Path Finder with edge definitions and path queries

--- Graph Path Finder/GraphPathFinder.pl
+++ Graph Path Finder/GraphPathFinder.pl
@@ -1,20 +1,21 @@
-        %************************************************************************************************************
-        %                                                                                                           *
-        %                                         Graph Path Finder                                                 *
-        %                                                                                                           *
-        %       This Prolog code defines a simple graph using edge facts and determines if there                    *
-        %       is a path between two nodes.                                                                        *
-        %       The 'edge' facts represent directed edges between nodes.                                            *
-        %       The 'path' rules define a path between two nodes X and Y.                                           *
-        %       The first rule states that there is a path from X to Y if there is a direct edge from X to Y.       *
-        %       The second rule states that there is a path from X to Y if there is a direct edge from X            *
-        %       to some node Z and there is a path from Z to Y.                                                     *
-        %                                                                                                           *
-        %                                          Example usage:                                                   *
-        %                                          ?- path(a, f).                                                   *
-        %                                          Expected: true                                                   *
-        %                                                                                                           *
-        %************************************************************************************************************
+%************************************************************************************************************
+%                                                                                                           *
+%                                         Graph Path Finder                                                 *
+%                                                                                                           *
+%       This Prolog code defines a simple graph using edge facts and determines if there                    *
+%       is a path between two nodes.                                                                        *
+%       The 'edge' facts represent directed edges between nodes.                                            *
+%       The 'path' rules define a path between two nodes X and Y.                                           *
+%       The first rule states that there is a path from X to Y if there is a direct edge from X to Y.       *
+%       The second rule states that there is a path from X to Y if there is a direct edge from X            *
+%       to some node Z and there is a path from Z to Y, ensuring that nodes are not revisited to prevent    *
+%       cycles.                                                                                             *
+%                                                                                                           *
+%                                          Example usage:                                                   *
+%                                          ?- path(a, f).                                                   *
+%                                          Expected: true                                                   *
+%                                                                                                           *
+%************************************************************************************************************
 
 
 
@@ -23,17 +24,24 @@
 
 edge(a, b).                                         % There is a direct edge from node 'a' to node 'b'
 edge(b, c).                                         % There is a direct edge from node 'b' to node 'c'
-edge(c, d).                                         % There is a direct edge from node 'c' to node 'd'
+edge(b, d).                                         % There is a direct edge from node 'b' to node 'd'
+edge(c, e).                                         % There is a direct edge from node 'c' to node 'e'
 edge(d, e).                                         % There is a direct edge from node 'd' to node 'e'
 edge(e, f).                                         % There is a direct edge from node 'e' to node 'f'
-edge(a, c).                                         % There is a direct edge from node 'a' to node 'c'
-edge(c, e).                                         % There is a direct edge from node 'c' to node 'e'
+edge(f, g).                                         % There is a direct edge from node 'f' to node 'g'
+edge(g, h).                                         % There is a direct edge from node 'g' to node 'h'
+edge(h, i).                                         % There is a direct edge from node 'h' to node 'i'
 
+path(X, Y) :- path(X, Y, []).                      % Rule to find a path between X and Y using an empty visited list
+path(X, Y, _) :- edge(X, Y).                       % Rule to define a direct path between X and Y
+path(X, Y, Visited) :-                             % Rule to find a path between X and Y with a non-empty visited list
+    edge(X, Z),                                    % There exists an edge from X to Z
+    \+ member(Z, Visited),                         % Z is not already visited (to prevent cycles)
+    path(Z, Y, [Z|Visited]).                       % Continue the path from Z to Y, adding Z to Visited
 
-path(X, Y) :- edge(X, Y).                           % There is a path from X to Y if there is a direct edge from X to Y
-path(X, Y) :- edge(X, Z), path(Z, Y).               % There is a path from X to Y if there is a direct edge from X to some node Z and there is a path from Z to Y
-
-
+member(X, [X|_Tail]).                              % Base case: X is the head of the list
+member(X, [_Head|Tail]) :-                         % Recursive case: X is a member of the Tail of the list
+    member(X, Tail). 
 
 
 
